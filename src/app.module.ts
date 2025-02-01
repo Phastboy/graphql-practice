e64076dd2aad242a54f9ff73d8f22b58c61ed2c2
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-01 22:36:39 +0000

    generated resources with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaService } from './prisma.service';
+import { BooksModule } from './books/books.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
     imports: [
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
         }),
+        BooksModule,
+        ReviewsModule,
     ],
     controllers: [AppController],
     providers: [AppService, PrismaService],
