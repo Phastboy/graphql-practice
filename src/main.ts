584cae90a11a489510d77a9839fdb6bc565631b6
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-01 19:46:05 +0100

    feat(bootstrap): enhance logging and display network address
    - Introduced `Logger` for improved logging.
    - Added a function to retrieve the network address using `os.networkInterfaces()`.
    - Enhanced logging to display the application's accessible network URL.
    - Ensured proper protocol selection based on the `NODE_ENV` variable.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
 import { NestFactory } from '@nestjs/core';
-import { ConsoleLogger } from '@nestjs/common';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { networkInterfaces } from 'os';
 
 async function bootstrap() {
-    const port=process.env.PORT ?? 8080;
+    const logger = new Logger('Bootstrap');
+    const port = process.env.PORT ?? 8080;
+
     const app = await NestFactory.create(AppModule, {
         logger: new ConsoleLogger({
             json: true,
             colors: true,
         }),
     });
+
+    const networkAddress = (): string | null => {
+        return (
+            Object.values(networkInterfaces())
+                .flat()
+                .find((info) => info?.family === 'IPv4' && !info.internal)
+                ?.address ?? null
+        );
+    };
+
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+
     await app.listen(port);
+    logger.log(`Network -> ${protocol}://${networkAddress()}:${port}`);
 }
+
 bootstrap();
