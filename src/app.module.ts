a6d2b567bcaadd967b499b273ea7aa279e0f0238
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-04 08:45:46 +0100

    book rsources added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { BooksModule } from './books/books.module';
 
 @Module({
     imports: [
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
         }),
+        BooksModule,
     ],
     controllers: [],
     providers: [AppService],
