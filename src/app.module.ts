27d54f5cca63a46afb3123f6bdd7eac700af9a37
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-07 18:30:40 +0100

    feat: move GraphQL setup to GraphqlModule
    - Removed the GraphQL configuration from `app.module.ts`.
    - Moved GraphQL configuration and setup to `graphql.module.ts`.
    - Kept `BooksModule` and `GraphqlModule` as the remaining imports in `app.module.ts`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { BooksModule } from './books/books.module';
-import { join } from 'path';
 import { GraphqlModule } from './graphql/graphql.module';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver,
-            typePaths: ['./**/*.graphql'],
-            definitions: {
-                path: join(process.cwd(), 'src/graphql.ts'),
-            },
-        }),
-        BooksModule,
-        GraphqlModule,
-    ],
+    imports: [BooksModule, GraphqlModule],
     controllers: [],
     providers: [],
 })
